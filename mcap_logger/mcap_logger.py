b0e19604162e42fffd0355e58d5cff2444bccea4
Kristof Kovacs <kristof.kovacs1996@gmail.com>
2024-10-06 18:13:11 +0200

    feat: log mcap messages on the console

--- mcap_logger/mcap_logger.py
+++ mcap_logger/mcap_logger.py
@@ -1,4 +1,5 @@
 import inspect
+import logging
 import os
 import tempfile
 import time
@@ -10,23 +11,23 @@ from foxglove_schemas_protobuf.Log_pb2 import Log
 from mcap_protobuf.writer import Writer
 from google.protobuf.timestamp_pb2 import Timestamp
 
-FORMAT = "[{asctime}] [{name}.{funcname}:{lineno}] [{levelname}] {message}"
+FORMAT = "[{asctime}] [{name}.{funcName}:{lineno}] [{levelname}] {message}"
 LOGGER_ROOT = Path(
     os.environ.get(
         "LOGGER_ROOT", Path(tempfile.gettempdir()) / "mcap_logger" / "log.mcap"
     )
 )
 
-
-
 class Topic:
-    def __init__(self, name: str, writer: Writer) -> None:
+    def __init__(self, name: str, writer: Writer, logger: logging.Logger = None) -> None:
         self.name = name
         self.writer = writer
+        self.logger = logger
 
     def write(self, message: Any) -> None:
+        if self.logger:
+            self.logger.debug(f"{self.name} topic:\n{message=}")
         timestamp = int(time.time() * 1_000_000_000)
-        print(f"saving message:\n{message}")
         self.writer.write_message(
             topic=self.name,
             message=message,
@@ -36,11 +37,11 @@ class Topic:
 
 
 class MCAPLogger:
-    def __init__(self, log_file: Path) -> None:
-        print(f"Creating logger for {log_file}")
+    def __init__(self, log_file: Path, logger: logging.Logger) -> None:
         self.log_file = open(str(log_file), "wb")
         self.writer = Writer(self.log_file)
         self.log_topic = Topic(name="/log", writer=self.writer)
+        self.logger = logger
 
     def __del__(self):
         self.writer.finish()
@@ -48,26 +49,31 @@ class MCAPLogger:
     def debug(self, message: str) -> None:
         previous_frame = inspect.currentframe().f_back
         traceback = inspect.getframeinfo(previous_frame)
+        self.logger.debug(message)
         self._write_log(level="DEBUG", message=message, traceback=traceback)
 
     def info(self, message: str) -> None:
         previous_frame = inspect.currentframe().f_back
         traceback = inspect.getframeinfo(previous_frame)
+        self.logger.info(message)
         self._write_log(level="INFO", message=message, traceback=traceback)
 
     def warning(self, message: str) -> None:
         previous_frame = inspect.currentframe().f_back
         traceback = inspect.getframeinfo(previous_frame)
+        self.logger.warning(message)
         self._write_log(level="WARNING", message=message, traceback=traceback)
 
     def error(self, message: str) -> None:
         previous_frame = inspect.currentframe().f_back
         traceback = inspect.getframeinfo(previous_frame)
+        self.logger.error(message)
         self._write_log(level="ERROR", message=message, traceback=traceback)
 
     def fatal(self, message: str) -> None:
         previous_frame = inspect.currentframe().f_back
         traceback = inspect.getframeinfo(previous_frame)
+        self.logger.fatal(message)
         self._write_log(level="FATAL", message=message, traceback=traceback)
 
     def _write_log(self, level: str, message: str, traceback: Traceback) -> None:
@@ -81,12 +87,26 @@ class MCAPLogger:
         self.log_topic.write(log_message)
 
     def topic(self, name: str) -> Topic:
-        return Topic(name, writer=self.writer)
+        return Topic(name, writer=self.writer, logger=self.logger)
 
 
-def get_logger(log_file: Path = LOGGER_ROOT) -> MCAPLogger:
+def get_logger(name: str, log_file: Path = LOGGER_ROOT, level: int = None) -> MCAPLogger:
     create_parent_directory_if_not_there(log_file)
-    return MCAPLogger(log_file)
+
+    console_handler = logging.StreamHandler()
+
+    formatter = logging.Formatter(FORMAT, style="{")
+    console_handler.setFormatter(formatter)
+
+    if not level:
+        level = os.environ.get("LOG_LEVEL", "WARNING").upper()
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    console_handler.setLevel(level)
+
+    logger.addHandler(console_handler)
+
+    return MCAPLogger(log_file, logger)
 
 
 def create_parent_directory_if_not_there(path: Path) -> None:
